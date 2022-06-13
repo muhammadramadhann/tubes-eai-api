bce82fefed03a92476d19de55d1cef767eb66a00
muhammadramadhann <ramadhannkurniawan@gmail.com>
2022-06-13 15:28:47 +0700

    Add Relationship to Employee Data

--- app/Models/Offwork.php
+++ app/Models/Offwork.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Offwork extends Model
 {
@@ -18,4 +19,19 @@ class Offwork extends Model
         'deskripsi',
         'status'
     ];
+
+    public function employee()
+    {
+        return $this->belongsTo(User::class, 'id_karyawan');
+    }
+
+    public function getTanggalCuti()
+    {
+        return Carbon::parse($this->attributes['tanggal_cuti'])->translatedFormat('j F Y');
+    }
+
+    public function getTanggalKembali()
+    {
+        return Carbon::parse($this->attributes['tanggal_kembali'])->translatedFormat('j F Y');
+    }
 }
